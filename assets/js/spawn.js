90b74cee6e0d2ba02075198a379945bb1d2c2f8d
Jirka Balhar <balhar.j@gmail.com>
2012-08-28 00:23:45 +0200

    Spawn price

--- assets/js/spawn.js
+++ assets/js/spawn.js
@@ -1,8 +1,9 @@
-function Spawn(con,x,side){
+function Spawn(con,x,side, price){
 	if(con !== undefined){
 		var obj = new con({});
 		obj.position = new Vector2(x,game.links.terrain.getHeight(x)-obj.height/2)
 		obj.owner = side;
+		obj.price = price;
 		if(side == "enemy"){
 			obj.texture.flip = "x";
 			game.ai.property.push(obj)
